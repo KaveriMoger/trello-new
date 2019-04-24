d745a3c4485bba9b061b91417dd46100e3c72774
Kaveri TM <kmoger@altimetrik.com>
2019-04-23 23:15:15 -0700

    components added

---
+++ src/components/trello/Doing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Doing extends React.Component {
+  render() {
+    return (
+      <p>Doing</p>
+    )
+  }
+}
+
+export default Doing
---
+++ src/components/trello/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+// import { NavLink } from "react-router-dom"
+import AddCard  from "../card/AddCard"
+
+class Todo extends React.Component {
+  render() {
+    return (
+      <div>
+      <p>To Do</p>
+
+      </div>
+    )
+  }
+}
+
+export default Todo
+;
